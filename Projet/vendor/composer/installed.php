d3638b526b11d7e1fc9ba67c768c5d26c1f12764
lucarafe <luc.bonin0903@gmail.com>
2022-11-07 14:33:11 +0100

    composer + vendor

---
+++ Projet/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9143c16929f421cd6cb7717719f9eb6fb0138ef3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9143c16929f421cd6cb7717719f9eb6fb0138ef3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
