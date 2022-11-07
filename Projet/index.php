bc6a6327b3419de893f2f918a999397143298c7b
lucarafe <luc.bonin0903@gmail.com>
2022-11-07 14:52:33 +0100

    index

--- Projet/index.php
+++ Projet/index.php
@@ -1,6 +1,45 @@
 <?php
+
 session_start();
 
 
 require_once 'vendor/autoload.php';
-use iutnc\netVOD\dispatcher\Dispatcher;
+
+
+\iutnc\deefy\db\ConnectionFactory::setConfig();
+
+use iutnc\deefy\dispatcher\Dispatcher;
+
+
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+
+if(!isset($playlists))
+{
+    $playlists = [];
+}
+
+$htmlRender = <<<EOF
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Deefy</title>
+    </head>
+    <body>
+    <nav>
+        <ul>
+            <li><a href="?action=add-user">Inscription</a></li>
+            <li><a href="?action=signin">Connexion</a></li>
+            <li><a href="?action=add-playlist">Ajouter une Playlist</a></li>
+            <li><a href="?action=display-playlist">Afficher une Playlist</a></li>
+        </ul>
+    </nav>
+
+EOF;
+
+$dispatcher = new Dispatcher($action);
+$dispatcher->renderPage($htmlRender);
+
+
