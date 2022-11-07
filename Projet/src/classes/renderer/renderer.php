8c4fba88d3ed5114c9e5d32e320ccc5f75290fc0
lucarafe <luc.bonin0903@gmail.com>
2022-11-07 15:46:22 +0100

    index

--- Projet/src/classes/renderer/renderer.php
+++ Projet/src/classes/renderer/renderer.php
@@ -4,5 +4,8 @@ namespace iutnc\netVOD\renderer;
 
 interface renderer
 {
+    const COMPACT = 1;
+    const LONG = 2;
 
+    public function render(int $selector): string;
 }
--- Projet/src/classes/video/track/Episode.php
+++ Projet/src/classes/video/track/Episode.php
@@ -2,7 +2,9 @@
 
 namespace iutnc\netVOD\video\track;
 
-class Episode
+class Episode extends video
 {
-
+    public function __construct(string $title, int $duration, int $number, string $source) {
+        parent::__construct($title, $duration, $number, $source);
+    }
 }
