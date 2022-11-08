b63b75961a64f0b70b7535d31eb615519ca7928f
Sionrv <101088525+Sionrv@users.noreply.github.com>
2022-11-08 14:45:33 +0100

    repair

--- Projet/src/classes/action/SigninAction.php
+++ Projet/src/classes/action/SigninAction.php
@@ -15,18 +15,12 @@ class SigninAction extends Action
         try
         {
             $user = Auth::authenticate($_POST['email'], $_POST['password']);
-<<<<<<< HEAD
             if (isset($user)){
               $html = "<h2>Connexion réussi</h2> <br>"."<a href=?action=catalogue>Catalogue</a>";
 
 }
-            //foreach ($user->getPlaylists() as $value)
-            //{
-            //    return (new AudioListRenderer($value))->render(1);
-            //}
-=======
+
             if (isset($user)) $html = "<h2>Connexion réussi</h2>";
->>>>>>> 18c0598e01e1ae002fba47337d18df18fb748ff1
 
         } catch (AuthException $e) {
             $html = "<h2>".$e->getMessage()."</h2>";
