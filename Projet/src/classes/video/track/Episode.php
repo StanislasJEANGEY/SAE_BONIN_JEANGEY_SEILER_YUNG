1b830a4e778cc9b94bdc7f550da2d9e9cc347ff6
JEANGEY Stanislas <jeangey1u@etu.univ-lorraine.fr>
2022-11-08 15:25:23 +0100

    while dans Episode.php

--- Projet/src/classes/video/track/Episode.php
+++ Projet/src/classes/video/track/Episode.php
@@ -8,18 +8,20 @@ class Episode extends video
 {
     protected $idSaison;
 
-    public function __construct(string $title, int $duration,string $resume, string $source, int $idS) {
-        parent::__construct($title, $duration,$resume, $source);
+    public function __construct(string $title, int $duration, string $resume, string $source, int $idS)
+    {
+        parent::__construct($title, $duration, $resume, $source);
         $this->idSaison = $idS;
     }
 
-    public static function getEpisode(string $id) : Episode
+    public static function getEpisode(string $id): Episode
     {
         $bd = ConnectionFactory::makeConnection();
         $requete = $bd->prepare("SELECT * FROM episode WHERE id = ?");
         $requete->bindParam(1, $id);
-        $data = $requete->fetch();
-        $episode = new Episode($data['titre'], $data['duree'], $data['resume'], $data['file'], $data['serie_id']);
+        while ($data = $requete->fetch()) {
+            $episode = new Episode($data['titre'], $data['duree'], $data['resume'], $data['file'], $data['serie_id']);
+        }
         return $episode;
     }
 }
