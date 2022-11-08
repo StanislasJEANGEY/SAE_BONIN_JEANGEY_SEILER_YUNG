7fd1d1cf589546275a9fdb8329d0c5d38dad7de7
Mathis Seiler <mathis.seiler8@etu.univ-lorraine.fr>
2022-11-08 15:16:00 +0100

    graphisme

--- Projet/index.php
+++ Projet/index.php
@@ -33,10 +33,9 @@ $htmlRender = <<<EOF
                 <link rel="stylesheet" href="index.css">
             </head>
             <ul>
-                <li><a href="?action=add-user">Inscription</a></li>
-                <li><a href="?action=signin">Connexion</a></li>
+                <li><a id="accueil" href="?action=add-user">Inscription</a></li>
+                <li><a id="accueil" href="?action=signin">Connexion</a></li>
             </ul>
-
 
 EOF;
 }else{
@@ -44,8 +43,11 @@ EOF;
           <head>
               <link rel="stylesheet" href="index.css">
           </head>
-          <a id="retour" href='index.php'>Retour a l'accueil</a>
-
+          <div id="mainReturn">
+            <a id="retour" href='index.php'>Retour a l'accueil</a>
+            <span>
+            <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          </div>
   EOF;
 }
 echo $htmlRender;
