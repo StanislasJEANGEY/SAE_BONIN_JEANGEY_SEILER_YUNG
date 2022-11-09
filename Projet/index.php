4665126dcef28cddefb415e1ad37265576f7dab5
Mathis Seiler <mathis.seiler8@etu.univ-lorraine.fr>
2022-11-09 13:09:31 +0100

    .

--- Projet/index.php
+++ Projet/index.php
@@ -42,7 +42,7 @@ EOF;
   if(isset($_GET['action']) == 'catalogue'){
     $htmlRender .= <<<EOF
             <div id="mainReturn">
-              <a id="retour" href='?action=accueil'>Retour en arrière</a>
+              <a id="retour" href='?action=signin'>Retour en arrière</a>
               <span>
               <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             </div>
--- Projet/src/classes/action/DisplayCatalogueAction.php
+++ Projet/src/classes/action/DisplayCatalogueAction.php
@@ -19,13 +19,15 @@ class DisplayCatalogueAction extends Action
         if(!isset($_SESSION['user'])) {
         return "<a href=?action=sign-in>Veuillez vous connecter</a>";
     } else {
-        $html = "<h1> Catalogue : </h1>";
+        $html = "<div id=catTitre><h1 id=Titrecatalogue> Catalogue : </h1> </div>";
         $query = "SELECT idSerie,titre,img FROM serie";
         $result = ConnectionFactory::makeConnection()->prepare($query);
         $result->execute();
         while($data = $result->fetch()){
+            $html .= "<div id=MainAfficherSerie>";
             $html .= "<h2>" .$data['titre']. "<h2>";
             $html .= "<a href='?action=serie&id=". $data['idSerie'] . "'><br><img src='". $data['img'] ."' width='300' height='300'></a><br>";
+            $html .= "</div>";
         }
     }
     return $html;
