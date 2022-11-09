39345e0cc456a7184baa696f628893cdb66748ab
lucarafe <luc.bonin0903@gmail.com>
2022-11-09 10:43:47 +0100

    TA MERE

--- Projet/src/classes/action/DisplayCatalogueAction.php
+++ Projet/src/classes/action/DisplayCatalogueAction.php
@@ -20,12 +20,12 @@ class DisplayCatalogueAction extends Action
         return "<a href=?action=sign-in>Veuillez vous connecter</a>";
     } else {
         $html = "<h1> Catalogue : </h1>";
-        $query = "SELECT id,titre,img FROM serie";
+        $query = "SELECT idSerie,titre,img FROM serie";
         $result = ConnectionFactory::makeConnection()->prepare($query);
         $result->execute();
         while($data = $result->fetch()){
             $html .= "<h2>" .$data['titre']. "<h2>";
-            $html .= "<a href='?action=serie&id=". $data['id'] . "'><br><img src='". $data['img'] ."' width='300' height='300'></a><br>";
+            $html .= "<a href='?action=serie&id=". $data['idSerie'] . "'><br><img src='". $data['img'] ."' width='300' height='300'></a><br>";
         }
     }
     return $html;
