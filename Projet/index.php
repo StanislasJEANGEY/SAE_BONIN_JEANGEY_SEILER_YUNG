6086f408037c185882a191c360740f26faf7c689
JEANGEY Stanislas <jeangey1u@etu.univ-lorraine.fr>
2022-11-09 10:10:06 +0100

    manage index

--- Projet/index.php
+++ Projet/index.php
@@ -17,40 +17,35 @@ $action = isset($_GET['action']) ? $_GET['action'] : null;
 
 
 
-if(!isset($playlists))
-{
-    $playlists = [];
+if (!isset($playlists)) {
+  $playlists = [];
 }
 
 $dispatcher = new Dispatcher();
 $dispatcher->run();
-$htmlRender = '';
+$htmlRender = '<head>
+              <link rel="stylesheet" href="index.css">
+              </head>';
 
-if(!isset($_GET['action'])){
 
-$htmlRender = <<<EOF
-            <head>
-                <link rel="stylesheet" href="index.css">
-            </head>
+if (!isset($_GET['action'])) {
+
+  $htmlRender .= <<<EOF
             <ul>
                 <li><a id="accueil" href="?action=add-user">Inscription</a></li>
                 <li><a id="accueil" href="?action=signin">Connexion</a></li>
             </ul>
 
 EOF;
-}else{
+} else {
 
-  $htmlRender = <<<EOF
-          <head>
-              <link rel="stylesheet" href="index.css">
-          </head>
+  $htmlRender .= <<<EOF
           <div id="mainReturn">
             <a id="retour" href='index.php'>Retour à l'accueil</a>
             <span>
             <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           </div>
   EOF;
-
 }
 
 echo $htmlRender;
