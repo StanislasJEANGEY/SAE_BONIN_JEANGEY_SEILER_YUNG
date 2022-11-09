83c63581ef5381816605214fa21f3f48fe609d00
Mathis Seiler <mathis.seiler8@etu.univ-lorraine.fr>
2022-11-09 10:11:14 +0100

    modif

--- Projet/index.php
+++ Projet/index.php
@@ -40,17 +40,30 @@ $htmlRender = <<<EOF
 EOF;
 }else{
 
-  $htmlRender = <<<EOF
-          <head>
-              <link rel="stylesheet" href="index.css">
-          </head>
-          <div id="mainReturn">
-            <a id="retour" href='index.php'>Retour à l'accueil</a>
-            <span>
-            <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          </div>
-  EOF;
+  if(isset($_GET['action']) === 'catalogue'){
+    $htmlRender = <<<EOF
+            <head>
+                <link rel="stylesheet" href="index.css">
+            </head>
+            <div id="mainReturn">
+              <a id="retour" href='SigninAction.php'>Retour en arrière</a>
+              <span>
+              <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            </div>
+        EOF;
+  }else{
 
+      $htmlRender = <<<EOF
+              <head>
+                  <link rel="stylesheet" href="index.css">
+              </head>
+              <div id="mainReturn">
+                <a id="retour" href='index.php'>Retour à l'accueil</a>
+                <span>
+                <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              </div>
+      EOF;
+    }
 }
 
 echo $htmlRender;
