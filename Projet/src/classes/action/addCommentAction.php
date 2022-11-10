fdf71f892172ae4eaf3ea024d30e05f9b5d4dadb
Sionrv <101088525+Sionrv@users.noreply.github.com>
2022-11-10 11:05:19 +0100

    Bouton comment

--- Projet/src/classes/action/addCommentAction.php
+++ Projet/src/classes/action/addCommentAction.php
@@ -13,9 +13,9 @@ class addCommentAction extends Action
     protected function postExecute(): string {
         if(isset($_SESSION['user'])) {
             $user = unserialize($_SESSION['user']);
-            $idserie = $_GET['idserie'];
-            $commentaire = $_GET['commentaire'];
-            $note = $_GET['note'];
+            $idserie = $_POST['idserie'];
+            $commentaire = $_POST['commentaire'];
+            $note = $_POST['note'];
             $user->ajouterCommentaire($idserie, $note, $commentaire);
             echo $_GET['url'];
             header('Location: ');
