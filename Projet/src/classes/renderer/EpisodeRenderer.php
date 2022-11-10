5d45d79a854c69fd8490a29c68641136c297c499
Sionrv <101088525+Sionrv@users.noreply.github.com>
2022-11-10 11:15:10 +0100

    Bouton comment

--- Projet/src/classes/renderer/EpisodeRenderer.php
+++ Projet/src/classes/renderer/EpisodeRenderer.php
@@ -26,7 +26,7 @@ class EpisodeRenderer implements renderer
                 $html .= "<div class='track'>" .
                     "<p><video controls autoplay id=vd src='video/{$this->episode->source}' type='video/mp4'></video></p>";
                 $html .= <<<EOF
-                            <form method="POST" action="?action=ajouterCommentaireAction&idSerie={$this->episode->idSerie}">
+                            <form method="POST" action="?action=ajouterCommentaireAction&idSerie={$this->episode->idSerie}&id={$this->episode->numero}">
 
                                 <input type="submit" value="Commenter">
                             </form>
